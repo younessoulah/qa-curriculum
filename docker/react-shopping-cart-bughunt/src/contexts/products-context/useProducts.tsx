60ad47859be3215b60b97ad8b428122a47b295d4
younessoulah <younessoullah45@gmail.com>
2026-07-17 16:09:09 +0100

    chore(SB-05): size filter substring-matches instead of exact-matching

--- docker/react-shopping-cart-bughunt/src/contexts/products-context/useProducts.tsx
+++ docker/react-shopping-cart-bughunt/src/contexts/products-context/useProducts.tsx
@@ -32,7 +32,7 @@ const useProducts = () => {
       if (filters && filters.length > 0) {
         filteredProducts = products.filter((p: IProduct) =>
           filters.find((filter: string) =>
-            p.availableSizes.find((size: string) => size === filter)
+            p.availableSizes.find((size: string) => filter.includes(size))
           )
         );
       } else {
